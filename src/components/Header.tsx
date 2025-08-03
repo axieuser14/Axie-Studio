5d8375828d67df629cc2799bccdd43dc31f18edf
axieuser14 <user14@jonkoping.site>
2025-08-03 15:00:16 +0200

    Fix Header component syntax error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,11 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div>Header Component</div>
+    </header>
+  );
+};
+
+export default Header;
